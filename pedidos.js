3313aca287cf8786f6110aaee5fa10ef194de97c
Mateus Falk <mateus.efalk@gmail.com>
2022-11-03 19:30:14 -0300

    Verifica acesso

---
+++ pedidos.js
@@ -0,0 +1,21 @@
+
+
+ const verificaAcesso = () => {
+    if (localStorage.getItem(LOGADO) == 'true') {
+        console.log('Logado');
+        window.location.href = 'pedidos.html'
+        return true;
+      } else {
+        console.log('Deslogado');
+        window.location.href = 'login.html'
+        return false;
+      }
+    
+}
+
+
+
+
+const limpaCampos = () => {
+    $
+}
